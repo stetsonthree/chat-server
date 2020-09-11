67692e9a974bfecdeb9eb51578f389ef78500697
Lee Stetson <stetsonthree@icloud.com>
2020-09-11 00:31:25 -0400

    Moves sockets into Threads
    - Creates threads and moves socket code into them

--- chat_server.rb
+++ chat_server.rb
@@ -2,11 +2,15 @@ require 'socket'
 
 server = TCPServer.new(2000)
 
-while true
-  connection = server.accept
+puts "Server running on port 2000..."
 
-  connection.print "What's your name? "
-  name = connection.gets.chomp
-  connection.puts "Welcome, #{name}!"
-  connection.close
+while true
+  tcp_socket = server.accept
+  Thread.new(tcp_socket) do |socket|
+    socket.print "What's your name? "
+    name = socket.gets.chomp
+    socket.puts "Welcome, #{name}!"
+    socket.close
+  end
+
 end
